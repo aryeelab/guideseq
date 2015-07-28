2e7889d2854292f39a2302e3f505181559235bc7
Ved Topkar <vedtopkar@gmail.com>
2015-07-28 16:08:56 -0400

    started writing tests

--- tests/test_guideseq.py
+++ tests/test_guideseq.py
@@ -12,16 +12,78 @@ import unittest
 
 from guideseq import guideseq
 
-
-class TestGuideseq(unittest.TestCase):
+class DemultiplexTestCase(unittest.TestCase):
 
     def setUp(self):
+        # do demultiplexing
         pass
 
-    def test_something(self):
+    def testIfDemultiplexed(self):
+        # See if equal to known demultiplexed output
         pass
 
     def tearDown(self):
+        # Delete temp output
+        pass
+
+
+class ConsolidateTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # do the consolidation
+        pass
+
+    def testIfConsolidated(self):
+        # Test if output equals consolidated output
+        pass
+
+    def tearDown(self):
+        # Delete temp output
+        pass
+
+
+class AlignmentTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # do the alignment
+        pass
+
+    def testIfAligned(self):
+        # Test if output equals expected alignment output
+        pass
+
+    def tearDown(self):
+        # Delete temp output
+        pass
+
+
+class OfftargetIdentificationTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # do the offtarget identification
+        pass
+
+    def testIfConsolidated(self):
+        # Test if output equals expected offtarget identification output
+        pass
+
+    def tearDown(self):
+        # Delete temp output
+        pass
+
+
+class BackgroundSubtractTestCase(unittest.TestCase):
+
+    def setUp(self):
+        # do the bedtools subtraction
+        pass
+
+    def testIfConsolidated(self):
+        # Test if output equals consolidated output
+        pass
+
+    def tearDown(self):
+        # Delete temp output
         pass
 
 
