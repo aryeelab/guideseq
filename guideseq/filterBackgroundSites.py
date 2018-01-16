e628c49f9c518f8f104fa5828190485ea4d51b83
Jose Malagon Lopez <josemalagonlopez@Joses-MacBook-Pro.local>
2018-01-15 22:19:30 -0500

    Correction on filterBackgroundSites

--- guideseq/filterBackgroundSites.py
+++ guideseq/filterBackgroundSites.py
@@ -1,7 +1,6 @@
 import subprocess
 import os
 
-
 def filterBackgroundSites(bedtools_path, sample_path, control_path, outfile):
     output_folder = os.path.dirname(outfile)
     if not os.path.exists(output_folder):
@@ -12,8 +11,7 @@ def filterBackgroundSites(bedtools_path, sample_path, control_path, outfile):
 
     sample_noHeader_command = "sed '1d' {0} > {1}".format(sample_path, sample_noHeader)
     control_noHeader_command = "sed '1d' {0} > {1}".format(control_path, control_noHeader)
-    clean_command = "rm {0} {1}".format(control_path, sample_path)
-
+    clean_command = "rm {0} {1}".format(control_noHeader, sample_noHeader)
     bedtools_filter_command = '{0} intersect -a {1} -b {2}'.format(bedtools_path, sample_noHeader, control_noHeader)
 
     subprocess.check_call(sample_noHeader_command, shell=True, env=os.environ.copy())
